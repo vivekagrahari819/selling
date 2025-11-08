6cc0518523a29f32e8b43591eb62d6b5d43f7fe2
Vivek Agrahari <110084463+vivekagrahari819@users.noreply.github.com>
2025-11-08 08:47:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./models/User');
+// var usersRouter = require('./routes/users'); // uncomment if you have this route
 
 var app = express();
 
@@ -22,6 +23,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/selling', {
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('DB Connection Error:', err));
 
+// Set up session
+app.use(session({
+  secret: 'secretKey123',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// ✅ Make session user available in all templates
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -29,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter); // only if needed
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,11 +52,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
