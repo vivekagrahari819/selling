f8b82e2ffd2384e06b30b069ea3216fab57be7ce
Vivek Agrahari <110084463+vivekagrahari819@users.noreply.github.com>
2025-11-23 14:38:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ var session = require('express-session');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users'); // uncomment if you have this route
+var app = express(); // ✅ app must be defined BEFORE using routes
 
-var app = express();
+// Routes
+const indexRouter = require('./routes/index');
+const productRoutes = require('./routes/productRoutes');
+// const usersRouter = require('./routes/users'); // use only if needed
 
-// view engine setup
+// View Engine Setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Connect to MongoDB
+// MongoDB Connection
 mongoose.connect('mongodb://127.0.0.1:27017/selling', {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -23,34 +25,39 @@ mongoose.connect('mongodb://127.0.0.1:27017/selling', {
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('DB Connection Error:', err));
 
-// Set up session
+// Session Setup
 app.use(session({
   secret: 'secretKey123',
   resave: false,
   saveUninitialized: false
 }));
 
-// ✅ Make session user available in all templates
+// Make session user available in all EJS files
 app.use((req, res, next) => {
   res.locals.user = req.session.user || null;
   next();
 });
 
+
+
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routes
 app.use('/', indexRouter);
-// app.use('/users', usersRouter); // only if needed
+app.use('/', productRoutes);
+// app.use('/users', usersRouter); // only if you create this route
 
-// catch 404 and forward to error handler
+// 404 Handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error Handler
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
