78ab4106d5310fc2bbc029e0a395caac0ab89bbe
Vivek Agrahari <110084463+vivekagrahari819@users.noreply.github.com>
2025-12-14 15:12:46 +0530

    Refactor app.js for middleware and route organization

--- app.js
+++ app.js
@@ -6,16 +6,7 @@ var session = require('express-session');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var app = express(); // ✅ app must be defined BEFORE using routes
-
-// Routes
-const indexRouter = require('./routes/index');
-const productRoutes = require('./routes/productRoutes');
-// const usersRouter = require('./routes/users'); // use only if needed
-
-// View Engine Setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+var app = express();
 
 // MongoDB Connection
 mongoose.connect('mongodb://127.0.0.1:27017/selling', {
@@ -25,20 +16,12 @@ mongoose.connect('mongodb://127.0.0.1:27017/selling', {
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('DB Connection Error:', err));
 
-// Session Setup
-app.use(session({
-  secret: 'secretKey123',
-  resave: false,
-  saveUninitialized: false
-}));
+// Models
+const Cart = require("./models/Cart");
 
-// Make session user available in all EJS files
-app.use((req, res, next) => {
-  res.locals.user = req.session.user || null;
-  next();
-});
-
-
+// View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Middlewares
 app.use(logger('dev'));
@@ -47,10 +30,41 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// Session Setup
+app.use(session({
+  secret: 'secretKey123',
+  resave: false,
+  saveUninitialized: false
+}));
+
+// ✅ Global middleware to pass user & cartCount to EJS
+app.use(async (req, res, next) => {
+    res.locals.user = req.session.user || null;
+
+    if (req.session.user) {
+        const cart = await Cart.findOne({ userId: req.session.user._id });
+        res.locals.cartCount = cart ? cart.products.length : 0;
+    } else {
+        res.locals.cartCount = 0;
+    }
+
+    next();
+});
+
+// Routes (after session & middlewares)
+const indexRouter = require('./routes/index');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const paymentRoutes = require("./routes/paymentRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const paymentStatusRoutes = require("./routes/paymentStatusRoutes");
+
 app.use('/', indexRouter);
 app.use('/', productRoutes);
-// app.use('/users', usersRouter); // only if you create this route
+app.use('/', cartRoutes);
+app.use('/', paymentRoutes);
+app.use('/', orderRoutes);
+app.use('/', paymentStatusRoutes);
 
 // 404 Handler
 app.use(function(req, res, next) {
