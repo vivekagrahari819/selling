a05312bb3b7ef3f0e48f7669d9b5aae7fdf8c84f
Vivek Agrahari <110084463+vivekagrahari819@users.noreply.github.com>
2025-12-14 15:04:37 +0530

    Add files via upload

---
+++ config/razorpay.js
@@ -0,0 +1,8 @@
+const Razorpay = require("razorpay");
+
+const razorpay = new Razorpay({
+    key_id: "rzp_test_qwUWbYhsbam1oJ",
+    key_secret: "NKuKFl5Q2PcW5poRLqXNaqOE"
+});
+
+module.exports = razorpay;
